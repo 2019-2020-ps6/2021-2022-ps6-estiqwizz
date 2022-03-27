d39599e8108ce70fa756aa3701b8a66f60b4c65a
Antoine Buquet <antoine.buquet49@gmail.com>
2022-03-27 18:37:49 +0200

    modif html ayman et passage du css en scss #1

--- front-end/src/app/parametre/parametre.component.ts
+++ front-end/src/app/parametre/parametre.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-parametre',
   templateUrl: './parametre.component.html',
-  styleUrls: ['./parametre.component.css']
+  styleUrls: ['./parametre.component.scss']
 })
 export class ParametreComponent {
 
