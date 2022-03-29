e325afc14e9679843ccd56a03d888ed931d1bcfb
Thomas Billequin <billequin.thomas@gmail.com>
2022-03-29 09:46:16 +0200

    ajout de l'ajout de quiz

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -6,12 +6,14 @@ import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { HomeComponent } from './home/home.component';
 import { ParametreComponent } from './parametre/parametre.component';
 import { GuideListComponent } from './guide/guide-list/guide-list.component';
+import { QuizFormComponent } from './quizzes/quiz-form/quiz-form.component';
 
 const routes: Routes = [
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
     {path: 'home', component: HomeComponent},
     {path: 'parametre', component: ParametreComponent},
+    {path: 'quiz-form', component: QuizFormComponent},
     {path: 'guide-list', component: GuideListComponent},
     {path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
