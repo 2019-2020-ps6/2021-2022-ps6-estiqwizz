dade913accdf85b2144d16d58eac66b97b206ded
Thomas Billequin <billequin.thomas@gmail.com>
2022-04-19 10:41:05 +0200

    amelioration scss

--- front-end/src/mocks/guide-list.mock.ts
+++ front-end/src/mocks/guide-list.mock.ts
@@ -12,6 +12,11 @@ const s01: Step = {
 };
 
 const Steps1: Step[] = [s00, s01];
+const Steps2: Step[] = [];
+const Steps3: Step[] = [];
+const Steps4: Step[] = [];
+const Steps5: Step[] = [];
+const Steps6: Step[] = [];
 
 const GUIDE1: Guide = {
     id: '0',
@@ -19,4 +24,34 @@ const GUIDE1: Guide = {
     steps: Steps1
 };
 
-export const GUIDE_LIST: Guide[] = [ GUIDE1 ];
+const GUIDE2: Guide = {
+    id: '1',
+    title: 'Comment jouer à un quiz ?',
+    steps: Steps2
+};
+
+const GUIDE3: Guide = {
+    id: '2',
+    title: 'Comment créer son quiz ?',
+    steps: Steps3
+};
+
+const GUIDE4: Guide = {
+    id: '3',
+    title: 'Et si je suis daltonien ?',
+    steps: Steps4
+};
+
+const GUIDE5: Guide = {
+    id: '4',
+    title: 'Comment enlever les images ?',
+    steps: Steps5
+};
+
+const GUIDE6: Guide = {
+    id: '5',
+    title: 'Comment espacer les éléments ?',
+    steps: Steps6
+};
+
+export const GUIDE_LIST: Guide[] = [ GUIDE1, GUIDE2, GUIDE3, GUIDE4, GUIDE5, GUIDE6,  ];
