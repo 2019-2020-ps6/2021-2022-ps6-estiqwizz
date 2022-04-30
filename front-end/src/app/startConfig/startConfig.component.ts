f9093ee683f01d13c6be80e2d591c92ce31cef61
Thomas Billequin <billequin.thomas@gmail.com>
2022-04-30 19:01:28 +0200

    ajout de l'utilisation des services

--- front-end/src/app/startConfig/startConfig.component.ts
+++ front-end/src/app/startConfig/startConfig.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Router} from '@angular/router';
+import {ParamService} from '../../services/parametre.service';
+import {FormGroup, FormBuilder} from '@angular/forms';
+
 
 @Component({
   selector: 'config-page',
@@ -8,23 +11,36 @@ import {Router} from '@angular/router';
 })
 export class StartConfigComponent implements OnInit {
 
-    constructor(private route: Router) { }
+
+    constructor(private route: Router, public paramService: ParamService ) { }
 
     ngOnInit() { }
 
     setClas() {
+        this.paramService.setSep(false);
+        this.paramService.setTaille(false);
+        this.paramService.setDalto("Défaut");
         this.navigate();
     }
 
     setDmla() {
+        this.paramService.setTaille(false);
+        this.paramService.setDalto("Défaut");
+        this.paramService.setSep(true);
         this.navigate();
     }
 
     setPres() {
+        this.paramService.setDalto("Défaut");
+        this.paramService.setTaille(true);
+        this.paramService.setSep(false);
         this.navigate();
     }
 
     setDalt() {
+        this.paramService.setSep(false);
+        this.paramService.setTaille(false);
+        this.paramService.setDalto("Deutéranopie");
         this.navigate();
     }
 
